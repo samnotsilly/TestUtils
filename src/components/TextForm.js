9ca7010fea11724b7acdd9934fec3d89e8096e97
Samarth <samarthjoisar7@gmail.com>
2022-06-27 12:09:46 -0400

    my first commit using vscode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,23 +4,23 @@ import PropTypes from 'prop-types'
 
 
 
-export default function TextForm(props) {
-    const handleUpperCase =() => {
-        // we added new text which we get from uer to the console
-        console.log("clicked to be uppercase"+ text);
-        // we defined variable and converted the text which we got  from user into upper text
-        let newText = text.toUpperCase();
-        // we then returned the variable inside the settext function so that it can print it 
-        setText(newText);
+// export default function TextForm(props) {
+//     const handleUpperCase =() => {
+//         // we added new text which we get from uer to the console
+//         console.log("clicked to be uppercase"+ text);
+//         // we defined variable and converted the text which we got  from user into upper text
+//         let newText = text.toUpperCase();
+//         // we then returned the variable inside the settext function so that it can print it 
+//         setText(newText);
 
-    }
-    const handleOnCHange =(event ) => {
-        setText(event.target.value)
+//     }
+//     const handleOnCHange =(event ) => {
+//         setText(event.target.value)
 
-    }
-    // this is use state hook which helps use to set a state for the text
-    const [text, setText] = useState("Enter text here");
-// setText("new text");// correct way to change the text
+//     }
+//     // this is use state hook which helps use to set a state for the text
+//     const [text, setText] = useState("Enter text here");
+// // setText("new text");// correct way to change the text
 
     return (
     <div>
